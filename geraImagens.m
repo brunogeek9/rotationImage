92c4f457de71f18a1da396f0060b0232ec89439e
brunogeek9 <brunogeek9@gmail.com>
2018-07-09 18:42:09 -0300

    Versão inicial terminada

--- geraImagens.m
+++ geraImagens.m
@@ -1,15 +1,13 @@
-pkg load image
+#Função que cria novas imagens
 function geraImagens(im,ang,qtdRot)
   fundo = zeros(size(im,1),size(im,2),3);
   fundo = im(1,1,:);
+  #sf = separaAzul(im);
   for k=1:qtdRot;
     rot = imrotate(im, ang, 'nearest','crop', 0);
     ang = ang + 10;
     fileName = strcat('output (',num2str(k),').jpg');  
-    imwrite(rot,nomeImagem);
-    filename = strcat('saida (',num2str(k),').jpg');;
-    #rot 
-  #  output = max(fundo,rot);
-    imwrite(rot, strcat('C:\Users\Aluno\Downloads\Imagens\saidas\',fileName)); 
+    #output = max(fundo,rot);
+    imwrite(rot, strcat('C:\Users\jamelli\Desktop\outputs\',fileName)); 
   endfor
 endfunction
